c9ab2c6c9d860a1e078557f462d1ee5d8e90cf98
Agnes Ahlman <ahlman.agnes@gmail.com>
2023-05-09 13:48:18 +0200

    testing commits and push to github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Link from "next/link";
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <>
-      <h1>What to eat</h1>
+      <h1 className={styles.title}>What to eat</h1>
 
       <Link href="/login"> Go to log in </Link>
     </>
