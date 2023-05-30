704e254656cedbd6ac7cda1b5b18760098e5ab1e
Agnes Ahlman <ahlman.agnes@gmail.com>
2023-05-30 16:39:18 +0200

    fixed type on props in singleRecipe

--- src/app/singlerecipe/[id]/page.tsx
+++ src/app/singlerecipe/[id]/page.tsx
@@ -1,15 +1,20 @@
 import { getRecipeById } from "@/db/recipe";
 
-export default async function SingleRecipe(props) {
+type SingleRecipeProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function SingleRecipe(props: SingleRecipeProps) {
   console.log(props.params.id);
   const recipe = await getRecipeById(props.params.id);
   return (
     <section>
       <h1>Single Recipe</h1>
-      <p>{recipe?.name}</p>
-      <p>{recipe?.ingridients}</p>
-      <p>{recipe?.description}</p>
+      <p>{recipe.name}</p>
+      <p>{recipe.ingridients}</p>
+      <p>{recipe.description}</p>
     </section>
   );
-  return <h1>Single Recipe</h1>;
 }
